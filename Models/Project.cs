ffd5abf96913a6bbb9c77abc0ea46628ac6099ff
quoj <133200203+quoj@users.noreply.github.com>
2024-10-08 12:28:42 +0700

    Add files via upload

---
+++ Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DMAWS_T2305M_TuanAnh.Attributes;
+namespace DMAWS_T2305M_TuanAnh.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+
+        [Required, StringLength(150, MinimumLength = 2)]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public DateTime ProjectStartDate { get; set; }
+
+        [CompareDates]
+        public DateTime? ProjectEndDate { get; set; } 
+
+        // Navigation property to handle many-to-many relation with Employee
+        public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set; }
+    }
+}
